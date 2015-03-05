9bbc32b7bf17fca687a7bacd4b9f28bf6e04d5a9
Kazunori Tamura <sunes232002@gmail.com>
2015-03-05 18:13:54 +0900

    sksファイルを使わないようにした

---
+++ minesweeper/GameGridNode.swift
@@ -0,0 +1,13 @@
+//
+//  GameGridNode.swift
+//  minesweeper
+//
+//  Created by tamura on 2015/03/05.
+//  Copyright (c) 2015年 tamura. All rights reserved.
+//
+
+import SpriteKit
+
+class GameGridNode: SKNode {
+    
+}
--- minesweeper/GameViewController.swift
+++ minesweeper/GameViewController.swift
@@ -30,20 +30,20 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let scene = GameScene()
+        // Configure the view.
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .AspectFill
+        scene.size = skView.frame.size
+        
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
